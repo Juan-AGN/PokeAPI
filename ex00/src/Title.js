1399e7238bf61eccaa0cd7e3a06330f5b796348c
Juan-AGN <142683644+Juan-AGN@users.noreply.github.com>
2024-10-22 19:41:02 +0200

    Update Title.js

--- ex00/src/Title.js
+++ ex00/src/Title.js
@@ -7,6 +7,9 @@ function App() {
     <br></br>
     <div class="titlebackground">
       <h1 class="title">PokeGenerator</h1>
+      <p>
+      MADE BY: Juan Antonio García Narváez (juan-ant)
+      </p>
     </div>
     <br></br>
     </div>
